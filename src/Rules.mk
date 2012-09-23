355b9c72afd5a21a287fab56ce71021d0008e609
Cyril Paulus <paulus.cyril@gmail.com>
2012-09-23 18:10:12 +0200

    Makefile fix

--- src/Rules.mk
+++ src/Rules.mk
@@ -1,17 +1,17 @@
 .PHONY: src_all src_clean src_distclean
 SUBDIRS := $(DIR) $(DIR)/entity $(DIR)/utils $(DIR)/network $(DIR)/ui $(DIR)/menu
 src_EXEC := $(DIR)/a.out
-src_DS := $(DIR)/ds
+src_DS := $(DIR)/dedi/ds
 src_SRCS := $(foreach D,$(SUBDIRS),$(wildcard $D/*.cpp))
 src_OBJS := $(src_SRCS:.cpp=.o)
 
-src_all: $(src_EXEC)
+src_all: $(src_EXEC) $(src_DS)
 
 $(src_EXEC): $(src_OBJS)
 	$(CC) $(CFLAGS) $(LDFLAGS) $(src_OBJS) -o $(src_EXEC)
 
 $(src_DS): $(src_OBJS)
-	$(CC) $(CFLAGS) dedi/dedi.cpp config.o server.o ticker.o network/*.o entity/*.o utils/*.o -lsfml-network -lsfml-system -lenet -o $(src_DC)
+	$(CC) $(CFLAGS) $(DIR)/dedi/dedi.cpp $(DIR)/config.o $(DIR)/server.o $(DIR)/ticker.o $(DIR)/network/*.o $(DIR)/entity/*.o $(DIR)/utils/*.o -lsfml-network -lsfml-system -lenet -o $(src_DS)
 
 %.o: %.cpp
 	$(CC) $(CFLAGS) -c $^ -o $@
