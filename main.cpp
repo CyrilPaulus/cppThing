bd737b660d1afaa5430398cd5c5d368050c2a0f4
Cyril Paulus <paulus.cyril@gmail.com>
2013-08-21 15:13:36 +0200

    Fixed argument

--- main.cpp
+++ main.cpp
@@ -7,14 +7,18 @@ int main (int argc, char *argv[]) {
   int gameType = Game::LOCAL;
 
   for(int i = 0; i < argc; i++){
-    if(strcmp(argv[i], "-server") == 0)
+    if(strcmp(argv[i], "--server") == 0)
       gameType = Game::SERVER;
-    else if (strcmp(argv[i], "-connect") == 0){
+    else if (strcmp(argv[i], "--connect") == 0) {
       gameType = Game::CLIENT;
       game.setIp(argv[i+1]);
-    }
-    else if(strcmp(argv[i], "-port") == 0){
+    } else if(strcmp(argv[i], "--port") == 0) {
       game.setPort(atoi(argv[i+1]));
+    } else if(strcmp(argv[i], "--help") == 0) {
+      std::cout << "\t --server \t : Create a server" << std::endl;
+      std::cout << "\t --connect ip \t : Connect to ip" << std::endl;
+      std::cout << "\t --port port \t : Specify port" << std::endl;
+      return EXIT_SUCCESS;
     }
   }
   printf("GAMETYPE : %d\n", gameType);
